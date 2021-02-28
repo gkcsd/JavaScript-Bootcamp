ba3fe781aad77c060950f0262bc5d5cabbe2ab03
Ganesh Kale <kaleg2025@gmail.com>
2021-02-28 15:52:20 +0530

    Var point added

---
+++ NodeJS Basics/01 Basics/Varibles.js
@@ -0,0 +1,16 @@
+let name = "John";
+
+let score = 100.2;
+let bonus = 20;
+
+let totalScore = score + bonus;
+
+console.log(totalScore);
+
+let firstName = "John";
+
+firstName = "Cena";
+
+let lastName = "Doe";
+
+console.log(firstName + " " + lastName);
