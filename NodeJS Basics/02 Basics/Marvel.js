051a0846d1c3b432d9f09800ff120ba6039c94af
Ganesh Kale <kaleg2025@gmail.com>
2021-02-28 18:30:41 +0530

    ArrayThree Operation Added

--- NodeJS Basics/02 Basics/Marvel.js
+++ NodeJS Basics/02 Basics/Marvel.js
@@ -5,3 +5,30 @@ console.log(superHeroes[2]);
 console.log(superHeroes[superHeroes.length - 1]);
 
 console.log(`There is ${superHeroes.length} Super Heroes`);
+
+let Numbers = ["One", "Two", "Three", "Four", "Five", "Six"];
+
+Numbers[1] = "Something";
+
+console.log(Numbers);
+
+console.log();
+//Start
+Numbers.shift();
+console.log(Numbers);
+
+Numbers.unshift("thisAdded");
+console.log(Numbers);
+
+console.log();
+// End
+Numbers.pop();
+console.log(Numbers);
+
+Numbers.push("Six");
+console.log(Numbers);
+
+console.log();
+// Middle
+Numbers.splice(2, 3, "MEAdded");
+console.log(Numbers);
