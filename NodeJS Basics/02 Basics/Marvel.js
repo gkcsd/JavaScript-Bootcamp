fde15835c4db96dbb2a4d8fa5c0e35d7b5a5acd5
Ganesh Kale <kaleg2025@gmail.com>
2021-02-28 17:35:21 +0530

    Array Examplle Added

---
+++ NodeJS Basics/02 Basics/Marvel.js
@@ -0,0 +1,7 @@
+let superHeroes = ["Iron Man", "Spiderman", "Capt. America", "Thor"];
+
+console.log(superHeroes);
+console.log(superHeroes[2]);
+console.log(superHeroes[superHeroes.length - 1]);
+
+console.log(`There is ${superHeroes.length} Super Heroes`);
