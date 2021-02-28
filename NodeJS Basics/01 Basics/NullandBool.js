05e9fe372e1e1b2713e0e926c752b9983245e0b2
Ganesh Kale <kaleg2025@gmail.com>
2021-02-28 16:14:40 +0530

    NullandBool prog added

---
+++ NodeJS Basics/01 Basics/NullandBool.js
@@ -0,0 +1,13 @@
+let temp;
+
+console.log("The current temperature is: " + temp); // Undefined
+
+let present = true;
+
+console.log(present);
+
+let myScore = 20;
+
+let totalScore = myScore < 50;
+
+console.log(totalScore);
