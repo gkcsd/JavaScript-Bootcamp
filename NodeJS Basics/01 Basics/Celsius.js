bc6a22d2b6f40934175503fde97cbecc5ca616de
Ganesh Kale <kaleg2025@gmail.com>
2021-02-28 15:52:52 +0530

    Fah to cel prog added

---
+++ NodeJS Basics/01 Basics/Celsius.js
@@ -0,0 +1,14 @@
+let score = 200;
+const bonus = 20;
+
+let totalScore = score * bonus;
+
+let cal = ((4 + 4) * 4) / 2;
+
+console.log(totalScore);
+console.log(cal);
+
+let tempInFahrenheit = 100;
+
+let celsius = ((tempInFahrenheit - 32) * 5) / 9;
+console.log(celsius);
