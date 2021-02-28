f7af978476e4039a6375a62fee6f91bb29ab8d95
Ganesh Kale <kaleg2025@gmail.com>
2021-02-28 17:35:10 +0530

    Local Global Scope Added

---
+++ NodeJS Basics/02 Basics/Scope.js
@@ -0,0 +1,10 @@
+let iAmGlobal = "someValue";
+
+if (true) {
+  let iAmLocal = "someMoreValue";
+  iAmGlobal = "Superman";
+  console.log(iAmGlobal);
+  console.log(iAmLocal);
+}
+
+console.log(iAmGlobal);
