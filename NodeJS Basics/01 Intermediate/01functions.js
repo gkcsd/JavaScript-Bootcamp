a6ddd79c8c3bacf8a5ceeb90c1ddc720017819dd
gkcsd <kaleg2025@gmail.com>
2021-03-01 16:51:48 +0530

    multi Parameter Passing

--- NodeJS Basics/01 Intermediate/01functions.js
+++ NodeJS Basics/01 Intermediate/01functions.js
@@ -3,3 +3,11 @@ let sayHello = function(name) {
     console.log(`Hey ${name}`);
 }
 sayHello("John");
+
+console.log();
+
+let fullNameMaker = function(firstName, lastName) {
+    console.log("Wel-come to CodeBucket");
+    console.log(`Happy to have you ${firstName} ${lastName}`);
+}
+fullNameMaker("John", "Doe");
