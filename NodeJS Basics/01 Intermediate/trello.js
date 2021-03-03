b687d013a1b03a74bde6b60e8cedcf81e2479135
gkcsd <kaleg2025@gmail.com>
2021-03-03 17:39:13 +0530

    Month Example Added

--- NodeJS Basics/01 Intermediate/trello.js
+++ NodeJS Basics/01 Intermediate/trello.js
@@ -1,8 +1,24 @@
-let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 console.log(days[2]);
 console.log();
 
 days.forEach((day, index) => {
     console.log(`Starts with ${index+1} -- ${day}`);
+})
+
+// Assignment no.1 
+
+const months = ['Jan', 'Feb', 'Mar', 'April', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+months.forEach((month, index) => {
+    console.log(`Starts with ${index} -- ${month}`);
+})
+console.log();
+// Assignment no. 2 
+
+const Todos = ['Gym', 'Study', 'Practice', 'Office', 'Practice'];
+
+Todos.forEach((todo, index) => {
+    console.log(`This is my ${index+1} todo:  ${todo}`)
 })
